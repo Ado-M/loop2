0b18b7e02c8c56f121bd900b5b27d24db449c9a3
Ado555 <116297586+Ado555@users.noreply.github.com>
2023-02-13 19:15:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// oppgave 1
+//let i = 'tall';
+//for (let i = 0; i < 51; i++) {
+//  console.log(i);
+//}
+// oppgave 2
+//var i = 50;
+//
+//for (var i = 0; i < 51; i+=2) {
+//console.log(i)
+//}
+// oppgave3
+//var i = 100;
+//
+//for (var i = 1; i <= 100; i+=3) {
+//console.log(i)
+//}
+//var i = 100;
+//
+//for (var i = 1; i <= 100; i+=5) {
+//console.log(i)
+//}
+// oppgave4
+//var i = 5;
+//if (i === 3) {
+//  for (var i = 1; i <= 100; i+=3) {
+//    console.log(i)
+//    }
+//    var i = 100;
+//}
+//else if(i === 5) {
+//  for (var i = 1; i <= 100; i+=5) {
+//    console.log(i)
+//}
+//}
+
